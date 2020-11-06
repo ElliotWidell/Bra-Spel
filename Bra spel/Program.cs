3a5fe7480aaf75afb37c30d81bda08214818a3b3
Elliot <elliot.widell@elev.ga.ntig.se>
2020-11-06 14:40:01 +0100

    andra commit

--- Bra spel/Program.cs
+++ Bra spel/Program.cs
@@ -5,12 +5,16 @@ namespace grafik
 {
     class Program
     {
+
         static void Main(string[] args)
         {
             string gamestate = "menu";
             float life = 3;
             float X = 0;
             float Y = 0;
+            float score = 0;
+
+
 
             Raylib.InitWindow(1200, 800, "bra spel");
             while (!Raylib.WindowShouldClose())
@@ -18,7 +22,7 @@ namespace grafik
                 Raylib.BeginDrawing();
                 if (gamestate == "menu")
                 {
-
+                    life = 3;
                     Raylib.ClearBackground(Color.LIGHTGRAY);
                     Raylib.DrawText("Bra spel", 350, 150, 120, Color.DARKBLUE);
 
@@ -51,8 +55,15 @@ namespace grafik
                         Y += 0.3f;
                     }
 
+
+
                     Raylib.ClearBackground(Color.WHITE);
-                    Raylib.DrawRectangle((int)X, (int)Y, 50, 50, Color.GREEN);
+                    Raylib.DrawRectangle((int)X, (int)Y, 50, 70, Color.GREEN);
+                    Raylib.DrawRectangle((int)X + 35, (int)Y + 50, 20, 25, Color.RED);
+                    Raylib.DrawRectangle((int)X - 5, (int)Y + 50, 20, 25, Color.RED);
+                    Raylib.DrawRectangle((int)X + 15, (int)Y + 10, 20, 25, Color.BLUE);
+                    score = score + 0.01f;
+                    Raylib.DrawText("Score: " + score, 20, 20, 40, Color.PINK);
 
                     if (life <= 0)
                     {
@@ -63,6 +74,14 @@ namespace grafik
                     {
                         gamestate = "gameover";
                     }
+                    if (Raylib.IsKeyPressed(KeyboardKey.KEY_O))
+                    {
+                        life = life - 1;
+
+
+                    }
+
+
                 }
                 if (gamestate == "gameover")
                 {
